d833955c7028a5d359467d3f62582adfaf4ead9e
Trevor Ali <130318126+TrevorA1i@users.noreply.github.com>
2023-05-15 01:29:49 +1200

    Update FinancialStatsAnalysis.sql

--- FinancialStatsAnalysis.sql
+++ FinancialStatsAnalysis.sql
@@ -9,59 +9,111 @@ Analyzing financial incomes
 
 select * from FinancialStatistics
 
-select round(sum(income),0)
+-- The Total Income
+select round(sum(income),0) 'Total Income'
 from FinancialStatistics
 
-select round(sum([Target Income]),0)
-from FinancialStatistics
 
+-- Target Income
+select round(sum([Target Income]),0) 'Target Income'
+from FinancialStatistics;
+
+
+-- Total Income by Target Income and Percentage Difference
+with Income as (
+select 
+	round(sum(income),0)'Total Income', 
+	round(sum([Target Income]),0) 'Target Income'
+from FinancialStatistics
+where year = 2021
+)
+select 
+	[Target Income], 
+	[Total Income],
+	round((([Target Income]-[Total Income])*100/[Target Income]),2)'%Difference'
+from income
+
+
+-- Overall month with the highest income in 3 years
 select 
 	Month, 
 	sum(income)
 from FinancialStatistics
 group by month
+order by 2 desc
 
+-- Top monthly income per year
+with monthlyincome as (
+select top 1 Month,	year, sum(income)'Income'
+from FinancialStatistics where year=2020 group by month, year order by 3 desc
+union all
+select top 1 Month,	year, sum(income)'Income'
+from FinancialStatistics where year=2021 group by month, year order by 3 desc
+union all
+select top 1 Month,	year, sum(income)'Income' 
+from FinancialStatistics where year=2022 group by month, year order by 3 desc
+union all
+select top 1 Month,	year, sum(income)'Income'
+from FinancialStatistics where year=2023 group by month, year order by 3 desc
+union all
+select top 1 Month,	year, sum(income)'Income'
+from FinancialStatistics where year=2024 group by month, year order by 3 desc
+)
+select * from monthlyincome
 
+-- Income Sources Analysis
 select 
 	[Income sources], 
-	round(sum(income),0)
+	round(sum(income),0)'Income'
 from FinancialStatistics
-group by [Income sources]
+where year = 2022
+group by [Income sources];
 
-
+with incomes as (
+    select [Income sources], [Income Breakdowns], SUM(income) as Income
+    from FinancialStatistics
+    where year = 2021
+    group by [Income sources], [Income Breakdowns]
+), total_income as (
+    select SUM(income) as TotalIncome
+    from FinancialStatistics
+    where year = 2021
+)
 select 
-	[Income sources] ,
-	[Income Breakdowns], 
-	round(sum(income),0)
-from FinancialStatistics
-group by [Income Breakdowns], [Income sources]
+    [Income sources], [Income Breakdowns],
+    Income,
+    round((Income / TotalIncome) * 100, 2) as IncomePercentage
+from incomes
+CROSS JOIN total_income
+order by 3 desc;
 
 
+-- Marketing Strategies
 select 
 	[Marketing Strategies], 
-	sum(income),
-	round((sum(income)/sum(sum(income)) over () * 100),2)
+	sum(income)'Income',
+	round((sum(income)/sum(sum(income)) over () * 100),2)'%of Total'
 from FinancialStatistics
 group by [Marketing Strategies]
 
 
-
+--- QTY
 select 
 	[Income sources],
-	sum(counts),
-	round((sum(counts)/sum(sum(counts)) over() * 100),0)
+	sum(counts)'QTY',
+	round((sum(counts)/sum(sum(counts)) over() * 100),0)'%of Total'
 from FinancialStatistics
 group by [Income sources]
 order by 3 desc
 
-
+-- Income by Country
 select 
-	country, 
-	round(sum(income),0)
+	Country, 
+	round(sum(income),0)'Income'
 from FinancialStatistics
-group by country
+group by country;
 
-
+-- Average monthly income
 with avgmonth as (
 select 
 	Month, 
